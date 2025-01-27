18895e6651f9dcc6fe64037828a0956efbcbc73d
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-01-27 07:14:21 +0100

    Removed 2.0.2 with mike 2.1.3

--- 2.0.2/advanced_topics/unplot/main.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git clean -dfX .
-unset STEPUP_ROOT
-stepup -n -w1 | sed -f ../../clean_stdout.sed > stdout.txt
--- 2.0.2/advanced_topics/unplot/plan.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from stepup.core.api import static
-from stepup.reprep.api import unplot
-
-static("plot.svg")
-unplot("plot.svg")

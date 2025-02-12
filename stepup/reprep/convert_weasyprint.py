d1f0e225d29f5b602da40d8b51ec9ded54611cdc
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2025-02-12 17:18:36 +0100

    Update copyright

--- stepup/reprep/convert_weasyprint.py
+++ stepup/reprep/convert_weasyprint.py
@@ -1,5 +1,5 @@
 # StepUp RepRep is the StepUp extension for Reproducible Reporting.
-# Copyright (C) 2024 Toon Verstraelen
+# © 2024–2025 Toon Verstraelen
 #
 # This file is part of StepUp RepRep.
 #
